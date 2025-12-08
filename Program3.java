a0c37076072649e9a967e5c26bd501de6c272fb2
Nithiya5 <nithiyaprabha.r2022cse@sece.ac.in>
2025-12-08 12:30:27 +0530

    program4

---
+++ Program3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		boolean res = UserMainCode.triplet(arr);
+		
+	}
+}
+
+class UserMainCode{
+	public static boolean triplet(int arr[]){
+		for(int i=0;i<arr.length - 3;i++){
+			if(arr[i])
+		}
+		
+	}
+}
